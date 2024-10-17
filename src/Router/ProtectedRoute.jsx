9fdf1355f1965ef51dbdc3cce79b6cd2a87182b7
AngelJosue12 <20210672@uthh.edu.mx>
2024-10-17 01:04:44 -0600

    asds

--- src/Router/ProtectedRoute.jsx
+++ src/Router/ProtectedRoute.jsx
@@ -1,21 +1,4 @@
 
-/**
- * import React, { useState, useEffect } from 'react';
-import { Outlet, Navigate } from 'react-router-dom';
-import { useAuth } from '../Components/Contexts/AuthContexts';
-import { message } from 'antd';
-
-const ProtectorRutas = ({user, children}) => {
-    
-    if(user == false){
-        return <Navigate to='/Login' />
-    }
-    return children ? children: <Outlet/>
-};
-
-export default ProtectorRutas;
-
-/** */
 
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
