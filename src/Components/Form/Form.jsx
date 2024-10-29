7f2d2bd94396b43a789bf8481531ac553ef5af24
AngelJosue12 <20210672@uthh.edu.mx>
2024-10-29 03:17:04 -0600

    diseno login bug

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -289,7 +289,6 @@ export default function Form() {
                   onChange={(e) => setEmail(e.target.value)}
                   onBlur={() => validateEmail(email)}
                   required
-                  type="email"
                   autoComplete="email"
                   className={`correo block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${emailError ? 'input-error' : ''}`}
                 />
