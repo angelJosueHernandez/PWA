4890688acf94ef9ccc1e11b9ea4eb5c0e7eace11
AngelJosue12 <20210672@uthh.edu.mx>
2024-11-15 02:04:29 -0600

    dsdcsc

--- src/main.jsx
+++ src/main.jsx
@@ -26,8 +26,7 @@ Sentry.init({
   ],
    profilesSampleRate: 1.0,
    environment: "production",
-   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+   
 });
 
 const container = document.getElementById('root');
